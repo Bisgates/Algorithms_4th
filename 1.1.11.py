8189579889f834207479bcec7125f4094c3fd392
Han Jialu <hanjialu0o0@gmail.com>
2018-04-21 01:41:59 +0800

    matrix

--- 1.1.11.py
+++ 1.1.11.py
@@ -1,9 +1,20 @@
 # 2018-4-19
 # Write a code fragment that prints the contents of a two-dimensional boolean array, using * to represent true and a space to represent false. Include row and column numbers.
 
-a = input('Input: ')
+def printBooleanArray(A):
+	for line in A:
+		for ele in line:
+			if ele:
+				print('*', end=''),
+			else:
+				print(' ', end=''),
+		print('')
 
-for i in range(int(a)):
-	
 
-print(a)
+#----------------TEST-----------------
+A = [[True, False, True, True], [True, False, False, False], [False, False, False, True]]
+B = [[True], [False]]
+
+printBooleanArray(A)
+print('---------')
+printBooleanArray(B)
